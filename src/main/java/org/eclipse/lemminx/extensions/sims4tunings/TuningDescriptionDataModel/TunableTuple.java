202d97449175f1a9ede58d57e4de72a7bbc1a3e1
Firegate1075 <linus.mueller03@gmail.com>
2026-02-17 15:51:47 +0100

    Add attributes for TunableTuple

--- src/main/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionDataModel/TunableTuple.java
+++ src/main/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionDataModel/TunableTuple.java
@@ -1,4 +1,84 @@
 package org.eclipse.lemminx.extensions.sims4tunings.TuningDescriptionDataModel;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import javax.annotation.Nullable;
+import java.util.Optional;
+
 public class TunableTuple implements ITunableElement{
+    // mandatory attributes
+
+    @JacksonXmlProperty(isAttribute = true, localName = "class")
+    private String className;
+
+    // optional attributes
+
+    @Nullable
+    @JacksonXmlProperty(isAttribute = true)
+    private String name;
+
+    @Nullable
+    @JacksonXmlProperty(isAttribute = true)
+    private String display;
+
+    @Nullable
+    @JacksonXmlProperty(isAttribute = true)
+    private String description;
+
+    @Nullable
+    @JacksonXmlProperty(isAttribute = true, localName = "Deprecated")
+    private Boolean deprecated;
+
+    @Nullable
+    @JacksonXmlProperty(isAttribute = true)
+    private String group;
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public Optional<Boolean> isDeprecated() {
+        return Optional.ofNullable(deprecated);
+    }
+
+    public void setDeprecated(@Nullable Boolean deprecated) {
+        this.deprecated = deprecated;
+    }
+
+    public Optional<String> getGroup() {
+        return Optional.ofNullable(group);
+    }
+
+    public void setGroup(@Nullable String group) {
+        this.group = group;
+    }
+
+    public Optional<String> getName() {
+        return Optional.ofNullable(name);
+    }
+
+    public void setName(@Nullable String name) {
+        this.name = name;
+    }
+
+    public Optional<String> getDisplay() {
+        return Optional.ofNullable(display);
+    }
+
+    public void setDisplay(@Nullable String display) {
+        this.display = display;
+    }
+
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(description);
+    }
+
+    public void setDescription(@Nullable String description) {
+        this.description = description;
+    }
+
 }
