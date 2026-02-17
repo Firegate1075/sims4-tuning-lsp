78e2564f3824cc256096be29a2d0c419fb2fc105
Firegate1075 <linus.mueller03@gmail.com>
2026-02-17 17:45:18 +0100

    Fix Instance attributes, add getters and setters

--- src/main/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionDataModel/Instance.java
+++ src/main/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionDataModel/Instance.java
@@ -4,21 +4,94 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Instance {
+    // mandatory attributes
+
     @JacksonXmlProperty(isAttribute = true, localName = "instance_type")
-    String instanceType;
+    private String instanceType;
+
     @JacksonXmlProperty(isAttribute = true, localName = "class")
-    String className;
+    private String className;
+
     @JacksonXmlProperty(isAttribute = true, localName = "module")
-    String moduleName;
+    private String moduleName;
+
+    // optional attributes
+
+    @Nullable
     @JacksonXmlProperty(isAttribute = true)
-    String description;
+    private String description;
+
+    @Nullable
+    @JacksonXmlProperty(isAttribute = true, localName = "instance_subclasses_only")
+    private Boolean instanceSubclassesOnly;
+
+    @Nullable
     @JacksonXmlProperty(isAttribute = true)
-    String parents;
+    private String parents;
+
+
+    // sub elements
 
     @JacksonXmlElementWrapper(useWrapping = false)
-    List<ITuningDescriptionElement> tunableElements;
+    private final List<ITuningDescriptionElement> tunableElements = new ArrayList<>();
+
+
+    public String getInstanceType() {
+        return instanceType;
+    }
+
+    public void setInstanceType(String instanceType) {
+        this.instanceType = instanceType;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getModuleName() {
+        return moduleName;
+    }
+
+    public void setModuleName(String moduleName) {
+        this.moduleName = moduleName;
+    }
+
+    public Optional<String> getDescription() {
+        return Optional.ofNullable(description);
+    }
+
+    public void setDescription(@Nullable String description) {
+        this.description = description;
+    }
+
+    public Optional<Boolean> getInstanceSubclassesOnly() {
+        return Optional.ofNullable(instanceSubclassesOnly);
+    }
+
+    public void setInstanceSubclassesOnly(@Nullable Boolean instanceSubclassesOnly) {
+        this.instanceSubclassesOnly = instanceSubclassesOnly;
+    }
+
+    public Optional<String> getParents() {
+        return Optional.ofNullable(parents);
+    }
+
+    public void setParents(@Nullable String parents) {
+        this.parents = parents;
+    }
+
+    public List<ITuningDescriptionElement> getTunableElements() {
+        return tunableElements;
+    }
 }
