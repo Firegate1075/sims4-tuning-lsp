37acb9999f5fd97570cb6c6d06812e5e067c8a81
Firegate1075 <linus.mueller03@gmail.com>
2026-02-17 18:20:45 +0100

    Add TdescFrag

---
+++ src/main/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionDataModel/TdescFrag.java
@@ -0,0 +1,30 @@
+package org.eclipse.lemminx.extensions.sims4tunings.TuningDescriptionDataModel;
+
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TdescFrag {
+    @JacksonXmlProperty(isAttribute = true)
+    private String name;
+
+    // sub elements
+
+    @JacksonXmlElementWrapper(useWrapping = false)
+    private final List<ITuningDescriptionElement> tunableElements = new ArrayList<>();
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<ITuningDescriptionElement> getTunableElements() {
+        return tunableElements;
+    }
+}
