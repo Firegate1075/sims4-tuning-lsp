4c83cee95b2ec5c5d55151a2c6aebf5efe705ab3
Linus <122754984+Firegate1075@users.noreply.github.com>
2026-02-18 12:05:20 +0100

    Add tests for registry

--- src/test/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionParserTest.java
+++ src/test/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionParserTest.java
@@ -7,13 +7,6 @@ public class TuningDescriptionParserTest {
     public void test_parse() {
         var parsedTuningDescriptions = TuningDescriptionParser.parseTuningDescriptionXML();
 
-        TuningDescriptionRegistry registry = new TuningDescriptionRegistry();
-
-        parsedTuningDescriptions.forEach(registry::addTuningDescription);
-        var r = registry.getClassElementByPath("statistics-statistic_conditions.HiddenOrShownCondition.Timing");
-        r = registry.getClassElementByPath("ui-ui_dialog.UiDialogResponse.UiDialogUiRequest");
-        r = registry.getClassElementByPath("ui_dialog_notification.UiDialogNotification.UiDialogNotificationLevel");
-        r = registry.getClassElementByPath("ui_dialog_notification.UiDialogNotification.UiDialogNotificationAutoDeleteReason");
-        System.out.println("hello");
+        assert(parsedTuningDescriptions.size() == 1764);
     }
 }
---
+++ src/test/java/org/eclipse/lemminx/extensions/sims4tunings/TuningDescriptionRegistryTest.java
@@ -0,0 +1,78 @@
+package org.eclipse.lemminx.extensions.sims4tunings;
+
+import org.eclipse.lemminx.extensions.sims4tunings.TuningDescriptionDataModel.InstanceElement;
+import org.eclipse.lemminx.extensions.sims4tunings.TuningDescriptionDataModel.TdescFrag;
+import org.eclipse.lemminx.extensions.sims4tunings.TuningDescriptionDataModel.TunableList;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TuningDescriptionRegistryTest {
+
+    @Test
+    void addTuningDescription() {
+        var parsedTuningDescriptions = TuningDescriptionParser.parseTuningDescriptionXML();
+
+        assert(parsedTuningDescriptions.size() == 1764);
+        TuningDescriptionRegistry registry = new TuningDescriptionRegistry();
+
+        // add all the tuning descriptions
+        parsedTuningDescriptions.forEach(registry::addTuningDescription);
+    }
+
+    @Test
+    void getInstanceElementByClassName() {
+        var parsedTuningDescriptions = TuningDescriptionParser.parseTuningDescriptionXML();
+
+        assert(parsedTuningDescriptions.size() == 1764);
+        TuningDescriptionRegistry registry = new TuningDescriptionRegistry();
+
+        // add all the tuning descriptions
+        parsedTuningDescriptions.forEach(registry::addTuningDescription);
+
+        // get one example
+        Optional<InstanceElement> buff_instance = registry.getInstanceElementByClassName("Buff");
+        assert(buff_instance.isPresent());
+        assert(buff_instance.get().getClassName().equals("Buff"));
+    }
+
+    @Test
+    void getClassElementByPath() {
+        var parsedTuningDescriptions = TuningDescriptionParser.parseTuningDescriptionXML();
+
+        assert(parsedTuningDescriptions.size() == 1764);
+        TuningDescriptionRegistry registry = new TuningDescriptionRegistry();
+
+        parsedTuningDescriptions.forEach(registry::addTuningDescription);
+        var r = registry.getClassElementByPath("statistics-statistic_conditions.HiddenOrShownCondition.Timing");
+        assert(r.isPresent());
+        assert(r.get().getName().equals("Timing"));
+        r = registry.getClassElementByPath("ui-ui_dialog.UiDialogResponse.UiDialogUiRequest");
+        assert(r.isPresent());
+        assert(r.get().getName().equals("UiDialogUiRequest"));
+        r = registry.getClassElementByPath("ui-ui_dialog_notification.UiDialogNotification.UiDialogNotificationLevel");
+        assert(r.isPresent());
+        assert(r.get().getName().equals("UiDialogNotificationLevel"));
+        r = registry.getClassElementByPath("ui-ui_dialog_notification.UiDialogNotification.UiDialogNotificationAutoDeleteReason");
+        assert(r.isPresent());
+        assert(r.get().getName().equals("UiDialogNotificationAutoDeleteReason"));
+    }
+
+    @Test
+    void getTdescFragByClassName() {
+        var parsedTuningDescriptions = TuningDescriptionParser.parseTuningDescriptionXML();
+
+        assert(parsedTuningDescriptions.size() == 1764);
+        TuningDescriptionRegistry registry = new TuningDescriptionRegistry();
+
+        // add all the tuning descriptions
+        parsedTuningDescriptions.forEach(registry::addTuningDescription);
+
+        // get one example
+        Optional<TdescFrag> tunableTestSet = registry.getTdescFragByClassName("TunableTestSet");
+        assert(tunableTestSet.isPresent());
+        assert(((TunableList) tunableTestSet.get().getTunableElements().getFirst()).getClassName().equals("TunableTestSet"));
+    }
+}
