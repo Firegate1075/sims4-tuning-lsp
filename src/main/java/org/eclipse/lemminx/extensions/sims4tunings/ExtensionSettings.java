490470af538c036aeedf0253bfdb42bef4177a65
Firegate1075 <linus.mueller03@gmail.com>
2026-05-06 21:14:42 +0200

    Add class for the extension settings

---
+++ src/main/java/org/eclipse/lemminx/extensions/sims4tunings/ExtensionSettings.java
@@ -0,0 +1,27 @@
+package org.eclipse.lemminx.extensions.sims4tunings;
+
+// TODO: make vscode extension, that registers the settings in vscode
+
+/**
+ * This class is used to store the settings of the extension.
+ */
+public class ExtensionSettings {
+
+    private String tuningDescriptionPath;
+
+    /**
+     * Retuns the path to the tuning description directory.
+     * @return the path that contains the tuning description files.
+     */
+    public String getTuningDescriptionPath() {
+        return tuningDescriptionPath;
+    }
+
+    /**
+     * Sets the path to the tuning description directory.
+     * @param tuningDescriptionPath the path that contains the tuning description files.
+     */
+    public void setTuningDescriptionPath(String tuningDescriptionPath) {
+        this.tuningDescriptionPath = tuningDescriptionPath;
+    }
+}
