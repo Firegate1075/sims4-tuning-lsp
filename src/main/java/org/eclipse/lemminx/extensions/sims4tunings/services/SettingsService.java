b64bedb420b43e7ea1f35f5f64225c6c7339bacc
Firegate1075 <linus.mueller03@gmail.com>
2026-05-06 23:37:26 +0200

    Add methods for updating and retrieving settings

--- src/main/java/org/eclipse/lemminx/extensions/sims4tunings/services/SettingsService.java
+++ src/main/java/org/eclipse/lemminx/extensions/sims4tunings/services/SettingsService.java
@@ -2,26 +2,45 @@ package org.eclipse.lemminx.extensions.sims4tunings.services;
 
 // TODO: make vscode extension, that registers the settings in vscode
 
+import org.eclipse.lemminx.extensions.sims4tunings.models.SettingsModel.RootSettings;
+import org.eclipse.lemminx.extensions.sims4tunings.models.SettingsModel.Sims4TuningSettings;
+import org.eclipse.lemminx.utils.JSONUtility;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
 /**
  * This class is used to store the settings of the extension.
  */
 public class SettingsService {
+    private static final Logger LOGGER = Logger.getLogger(SettingsService.class.getName());
 
-    private String tuningDescriptionPath;
+    private static final String PROJECT_DIRECTORY = System.getProperty("user.dir");
+    Path DEFAULT_TDESC_PATH = Paths.get(PROJECT_DIRECTORY + "/tdesc");
+
+    private Sims4TuningSettings settings;
 
     /**
-     * Retuns the path to the tuning description directory.
-     * @return the path that contains the tuning description files.
+     * Updates the settings of the extension from the given XML settings object.
+     * @param xmlSettings the XML settings object.
      */
-    public String getTuningDescriptionPath() {
-        return tuningDescriptionPath;
+    public void updateSettings(Object xmlSettings) {
+        RootSettings rootSettings = JSONUtility.toModel(xmlSettings, RootSettings.class);
+        if (rootSettings != null) {
+            settings = JSONUtility.toModel(rootSettings.getSims4tunings(), Sims4TuningSettings.class);
+        }
     }
 
     /**
-     * Sets the path to the tuning description directory.
-     * @param tuningDescriptionPath the path that contains the tuning description files.
+     * Returns the path to the tuning description directory.
+     * @return the path to the tuning description directory.
      */
-    public void setTuningDescriptionPath(String tuningDescriptionPath) {
-        this.tuningDescriptionPath = tuningDescriptionPath;
+    public Path getTdescPath() {
+        if (settings != null && settings.getTdescPath() != null) {
+            return Paths.get(settings.getTdescPath());
+        } else {
+            return DEFAULT_TDESC_PATH;
+        }
     }
 }
